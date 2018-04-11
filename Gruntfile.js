c9179c96af4d7b23e35f44d4ce43f2e1c65c5894
Nicole <nicolecar14@gmail.com>
2018-04-10 23:49:32 -0700

    some work on v2

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,8 @@ module.exports = function(grunt){
 		sass: {
     		dist: {
       			files: {
-       				'public/css/main.css': 'sass/main.sass'
+       				'public/css/main.css': 'sass/main.sass',
+       				'public/css/v2.css': 'sass/v2.sass'
       			}
     		}
   		},
